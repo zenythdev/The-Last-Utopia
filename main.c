588cd0cd8dc4e4e2d769f3df7f7d281bba6a1521
zenythdev <125770194+zenythdev@users.noreply.github.com>
2023-05-11 22:35:04 -0600

    Deleted a few unnecessary files and removed previous texture managing system.

--- main.c
+++ main.c
@@ -14,10 +14,6 @@ void LoadTextures(void);
 void UnloadTextures(void);
 void UpdateDrawFrame(void);
 void AnimatePlayer(void);
-Texture player_textures[4][8] = {};
-Texture snake_textures[2][4] = {};
-Texture menu_backgrounds[5] = {};
-Texture tile_images[] = {};
 int menu_fade = 0;
 char * fade = "in";
 char * menu = "nexxus";
@@ -49,6 +45,8 @@ int main(void) {
 	Texture tile_water = LoadTexture("data/images/tile_water.png");
 	Texture nexxus_games = LoadTexture("data/images/nexxus_games.png");
 
+	Texture tile_img;
+
 	while (!WindowShouldClose()) {
 		UpdateMusicStream(song);
 		BeginDrawing();
@@ -56,17 +54,17 @@ int main(void) {
 			for (int i = 0; i < map.rows; i++) {
 				for (int j = 0; j < map.cols; j++) {
 					switch (map.data[i][j]) {
-						case 1:
-							DrawTextureEx(tile_grass, (Vector2){ j*30, i*30 }, 0.0f, 2.0f, RAYWHITE);
-							break;
 						case 0:
-							DrawTextureEx(tile_water, (Vector2){ j*30, i*30 }, 0.0f, 2.0f, RAYWHITE);
+							tile_img = tile_water;
+							break;
+						case 1:
+							tile_img = tile_grass;
 							break;
 					}
+					DrawTextureEx(tile_img, (Vector2){ j*30, i*30 }, 0.0f, 2.0f, RAYWHITE);
 				}
 			}
 			DrawFPS(10, 10);
-			DrawTexture(player_textures[player.facing][player.anim], 400, 225, RAYWHITE);
 			//if (menu == "nexxus") {
 			//	DrawTexture(nexxus_games, 0, 0, (Color){ menu_fade, menu_fade, menu_fade, 255});
 			//	if (menu_fade < 255) {
@@ -84,32 +82,11 @@ int main(void) {
 }
 
 void LoadTextures(void) {
-	char player_texture_names[4][8][15] = { { "ju1.png", "ju2.png", "ju3.png", "ju4.png", "ju5.png", "ju6.png", "ju7.png", "ju8.png" }, { "jl1.png", "jl2.png", "jl3.png", "jl4.png", "jl5.png", "jl6.png", "jl7.png", "jl8.png" }, { "jd1.png", "jd2.png", "jd3.png", "jd4.png", "jd5.png", "jd6.png", "jd7.png", "jd8.png" }, { "jr1.png", "jr2.png", "jr3.png", "jr4.png", "jr5.png", "jr6.png", "jr7.png", "jr8.png" } };
-	char snake_texture_names[2][4][15] = { { "snakel1.png", "snakel2.png", "snakel3.png", "snakel4.png" }, { "snaker1.png", "snaker2.png", "snaker3.png", "snaker4.png" } };
-	for (int i = 0; i < sizeof(player_texture_names) / sizeof(player_texture_names[0]); i++) {
-		for (int j = 0; j < sizeof(player_texture_names[0]) / sizeof(player_texture_names[0][0]); j++) {
-			char image_path[27] = "data/images/";
-			strcat(image_path, player_texture_names[i][j]);
-			player_textures[i][j] = LoadTexture(image_path);
-		}
-	}
-	for (int i = 0; i < sizeof(snake_texture_names) / sizeof(snake_texture_names[0]); i++) {
-		for (int j = 0; j < sizeof(snake_texture_names[0]) / sizeof(snake_texture_names[0][0]); j++) {
-			char image_path[27] = "data/images/";
-			strcat(image_path, snake_texture_names[i][j]);
-			snake_textures[i][j] = LoadTexture(image_path);
-		}
-	}
-	Texture menu_backgrounds[5] = { LoadTexture("data/images/menu_items.png"), LoadTexture("data/images/menu_equips.png"), LoadTexture("data/images/menu_journal.png"), LoadTexture("data/images/menu_crafting.png"), LoadTexture("data/images/menu_settings.png") };
+
 }
 
 void UnloadTextures(void) {
-	for (int i = 0; i < sizeof(player_textures) / sizeof(player_textures[0]); i++) {
-		for (int j = 0; j < sizeof(player_textures[0]) / sizeof(player_textures[0][0]); j++) {
-			UnloadTexture(player_textures[i][j]);
-		}
-	}
-	for (int i = 0; i < sizeof(menu_backgrounds) / sizeof(menu_backgrounds[0]); i++) UnloadTexture(menu_backgrounds[i]);
+
 }
 
 void UpdateDrawFrame(void) {
