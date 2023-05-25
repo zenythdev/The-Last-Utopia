4a4ced7b224e58348206d5664bad624fb3869415
zenythdev <125770194+zenythdev@users.noreply.github.com>
2023-05-24 19:47:45 -0600

    Added a snake enemy (although it does not quite function yet).
    Yes, I'm aware that it closes the game if you come into contact with a snake. This is not unintentional. I just had no other way to check whether collisions worked.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "raylib.h"
 #include "tiles.h"
 
@@ -17,6 +18,7 @@ Texture nexxus_games;
 Texture menu_images;
 Texture james_images;
 Texture player_images;
+Texture snake_images;
 Texture hotbar;
 
 typedef struct {
@@ -28,7 +30,25 @@ typedef struct {
 	int anim;
 	int sprite;
 	Movements moving;
+	Rectangle hitbox;
+	int iframes;
 } Player;
+
+struct obj_enemy {
+	float x;
+	float y;
+	int maxhp;
+	int hp;
+	float speed;
+	int facing;
+	int anim;
+	int sprite;
+	Rectangle hitbox;
+};
+
+struct obj_enemy obj_tattlesnake = { 0.0f, 0.0f, 45, 45, 0.4, 0, 0, 0, (Rectangle){ 0.0f, 0.0f, 0.0f, 0.0f } };
+
+struct obj_enemy enemies[20];
 
 typedef struct {
 	int age;
@@ -54,13 +74,18 @@ void LoadTextures(void);
 void UnloadTextures(void);
 void UpdateDrawFrame(void);
 Player AnimatePlayer(Player player);
+struct obj_enemy AnimateEnemy(struct obj_enemy enemy);
 
 int main(void) {
 
 	GameScreen currentScreen = GAMEPLAY;
 
+	for (int i = 0; i < 20; i++) {
+		enemies[i] = obj_tattlesnake;
+	}
+
 	Map map = LoadMap("data/maps/map1.txt");
-	Player player = { 0, 0, 2.0f, {0.0f, 0.0f }, 2, 0, 0, STILL };
+	Player player = { 0, 0, 2.0f, {0.0f, 0.0f }, 2, 0, 0, STILL, (Rectangle){ 383, 170, 37, 54 }, 40 };
 
 	InitWindow(screenWidth, screenHeight, "The Last Utopia");
 	InitAudioDevice();
@@ -86,6 +111,12 @@ int main(void) {
 				if (player.velocity[1] >= 0) player.velocity[1] -= player.speed / 20;
 				player.x += player.velocity[0];
 				player.y += player.velocity[1];
+				for (int i = 0; i < 20; i++) {
+					if (enemies[i].facing == 1) enemies[i].x += enemies[i].speed;
+					if (enemies[i].facing == 0) enemies[i].x -= enemies[i].speed;
+					enemies[i].hitbox = (Rectangle){ enemies[i].x + 4 - player.x, enemies[i].y + 22 - player.y, 24, 8 };
+					if (CheckCollisionRecs(player.hitbox, enemies[i].hitbox) == true) CloseWindow();
+				}
 				break;
 			default:
 				break;
@@ -106,7 +137,13 @@ int main(void) {
 				}
 			}
 			player = AnimatePlayer(player);
-			DrawTexturePro(james_images, (Rectangle){37 * player.sprite, 54 * player.facing, 37, 54 }, (Rectangle){ 383, 170, 37, 54 }, (Vector2){ 0, 0 }, 0.0f, RAYWHITE);
+			DrawTexturePro(james_images, (Rectangle){ 37 * player.sprite, 54 * player.facing, 37, 54 }, player.hitbox, (Vector2){ 0, 0 }, 0.0f, RAYWHITE);
+			DrawRectangleLinesEx(player.hitbox, 1.0f, SKYBLUE);
+			for (int i = 0; i < 20; i++) {
+				DrawTexturePro(snake_images, (Rectangle){ 15 * enemies[i].sprite, 15 * enemies[i].facing, 15, 15 }, (Rectangle){ enemies[i].x - player.x, enemies[i].y - player.y, 30, 30 }, (Vector2){ 0, 0 }, 0.0f, RAYWHITE);
+				enemies[i] = AnimateEnemy(enemies[i]);
+				DrawRectangleLinesEx(enemies[i].hitbox, 1.0f, RED);
+			}
 			DrawTexture(hotbar, 1.0f, 419.0f, RAYWHITE);
 			DrawFPS(10, 10);
 		EndDrawing();
@@ -120,6 +157,7 @@ void LoadTextures(void) {
 	tile_grass = LoadTexture("data/images/tile_grass.png");
 	tile_water = LoadTexture("data/images/tile_water.png");
 	james_images = LoadTexture("data/images/james.png");
+	snake_images = LoadTexture("data/images/snake.png");
 	menu_images = LoadTexture("data/images/menu.png");
 	hotbar = LoadTexture("data/images/hotbar.png");
 }
@@ -153,3 +191,16 @@ Player AnimatePlayer(Player player) {
 	}
 	return player;
 }
+
+struct obj_enemy AnimateEnemy(struct obj_enemy enemy) {
+	enemy.anim++;
+	if (enemy.anim > 7) {
+		enemy.anim = 0;
+		enemy.sprite++;
+		if (enemy.sprite >= 4) {
+			enemy.sprite = 0;
+		}
+	}
+	return enemy;
+}
+
